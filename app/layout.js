8ca976efc4b803a26c9f49c78af0975387d8afe2
Tanmay Waykar <87329832+tanmay2626@users.noreply.github.com>
2024-01-02 18:52:42 +0530

    Add Apple's SF Pro font

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const myFont = localFont({ src: "../apple-sfpro.woff" });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={myFont.className}>{children}</body>
     </html>
   );
 }
