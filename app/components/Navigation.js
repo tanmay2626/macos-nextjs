0409507fe4553b8072a3c050d2c11f13fd6c7d42
Tanmay Waykar <87329832+tanmay2626@users.noreply.github.com>
2024-01-09 11:46:54 +0530

    Update Navigation for Home

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -2,16 +2,36 @@ import Image from "next/image";
 import React, { useState, useEffect } from "react";
 import wifiImg from "../../public/wifi.png";
 import batteryImg from "../../public/battery.png";
+import searchImg from "../../public/search.png";
+import switchImg from "../../public/switch.png";
 
 const Navigation = (props) => {
   const [date, setDate] = useState(null);
+  const [time, setTime] = useState(null);
 
   const calcDate = () => {
     const currentDate = new Date();
 
-    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+    const monthsOfYear = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    const date = currentDate.getDate();
     const dayName = daysOfWeek[currentDate.getDay()];
+    const month = monthsOfYear[currentDate.getMonth()];
 
     let hours = currentDate.getHours();
     const minutes = currentDate.getMinutes();
@@ -20,11 +40,13 @@ const Navigation = (props) => {
     hours = hours % 12;
     hours = hours ? hours : 12;
 
-    const formattedTime = `${dayName} ${hours}:${
+    const formattedDate = `${dayName} ${month} ${date} `;
+    const formattedTime = `${hours} : ${
       minutes < 10 ? "0" + minutes : minutes
     } ${ampm}`;
 
-    setDate(formattedTime);
+    setDate(formattedDate);
+    setTime(formattedTime);
   };
 
   useEffect(() => {
@@ -37,11 +59,13 @@ const Navigation = (props) => {
         justifyContent: "flex-end",
         padding: "5px 10px",
         gap: 15,
+        backgroundColor: "black",
+        opacity: 0.1,
       }}
     >
       <Image
         src={batteryImg}
-        alt="wifi-icon"
+        alt="battery-icon"
         width={30}
         height={30}
         style={{ marginTop: -4 }}
@@ -53,7 +77,22 @@ const Navigation = (props) => {
         height={25}
         style={{ marginTop: -2 }}
       />
-      <h3>{date}</h3>
+      <Image
+        src={switchImg}
+        alt="switch-icon"
+        width={25}
+        height={25}
+        style={{ marginTop: -2 }}
+      />
+      <Image
+        src={searchImg}
+        alt="search-icon"
+        width={25}
+        height={25}
+        style={{ marginTop: -2 }}
+      />
+      <h3 style={{ wordSpacing: 5 }}>{date}</h3>
+      <h3>{time}</h3>
     </div>
   );
 };
--- app/home/layout.js
+++ app/home/layout.js
@@ -2,6 +2,8 @@
 
 import Navigation from "../components/Navigation";
 import localFont from "next/font/local";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 const myFont = localFont({ src: "../../apple-sfpro.woff" });
 
@@ -10,6 +12,14 @@ export default function HomeLayout({ children }) {
     <html lang="en">
       <body className={myFont.className}>
         <div>
+          <Image
+            className={styles.homeBg}
+            alt="wallpaper"
+            src="/wallpaper7.jpg"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+          />
           <Navigation />
           {children}
         </div>
